fdc7b5cf0c4a796edfb115fbe01be4fc4876baa7
globaldyne <jbelekios@gmail.com>
2022-08-05 21:02:46 +0100

    Add support for password reset for older versions

--- reset_pass.sh
+++ reset_pass.sh
@@ -2,16 +2,22 @@
 #
 #
 # Reset admin pass
-# Script Version: v1.1
+# Script Version: v1.2
 # Author: John Belekios <john@globaldyne.co.uk>
 #
 #
 
-
 PASS=$(openssl rand -hex 8)
+VER=$(cat /var/www/html/VERSION.md)
 
-mysql -h localhost -upvault -ppvault pvault -e \
-  "UPDATE users SET password = '$PASS' WHERE id = '1';"
+if (( $(echo "$VER > 4.7" | bc -l) )); then
+        mysql -h localhost -upvault -ppvault pvault -e \
+                "UPDATE users SET password = '$PASS' WHERE id = '1';"
+
+else
+        mysql -h localhost -upvault -ppvault pvault -e \
+                "UPDATE users SET password = PASSWORD('$PASS') WHERE id = '1';"
+fi
 
 clear
 echo New Password is: $PASS
